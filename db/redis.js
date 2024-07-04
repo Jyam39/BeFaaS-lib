c8b45532c8be3e2a52518c29ca7dedb029841866
JunyaYamamoto <jyam3939@gmail.com>
2024-07-04 15:24:51 +0900

    add: Redisの認証方法を変更

--- db/redis.js
+++ db/redis.js
@@ -1,7 +1,11 @@
 const Redis = require('ioredis')
 
 module.exports = function redis () {
-  const db = new Redis(process.env.REDIS_ENDPOINT)
+  const db = new Redis({
+    host: 'release-name-redis-master.redis.svc.cluster.local',
+    port: 6379,
+    password: "lJKM6cwRN4",
+  })
   return {
     set: async (key, value) => {
       return db.set(key, JSON.stringify(value))
